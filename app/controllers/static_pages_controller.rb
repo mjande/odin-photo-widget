d354d5341c2558c9cf909e557934ee5c3ea7ad3d
mattand <mjande@umich.edu>
2022-08-17 15:12:33 -0400

    Add search by username function

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,11 +2,12 @@ require 'flickr'
 
 class StaticPagesController < ApplicationController
   def index
-    return unless params[:user_id]
+    return unless params[:username]
 
     flickr = Flickr.new
-    @user = flickr.people.getInfo user_id: params[:user_id]
-    @photos = flickr.people.getPublicPhotos user_id: params[:user_id]
+    user_id = flickr.people.findByUsername(username: params[:username]).id
+    @user = flickr.people.getInfo(user_id:)
+    @photos = flickr.people.getPublicPhotos(user_id:)
     @photo_urls = @photos.map { |photo| Flickr.url_m(photo) }
   end
 end
